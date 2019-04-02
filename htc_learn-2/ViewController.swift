8105120ac7347461de13a50366a260a76fa4ea3c
miromax <miromax21@gmail.com>
2019-04-02 19:15:38 +0400

    Initial Commit

---
+++ htc_learn-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  htc_learn-2
+//
+//  Created by maxim mironov on 02/04/2019.
+//  Copyright © 2019 maxim mironov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
